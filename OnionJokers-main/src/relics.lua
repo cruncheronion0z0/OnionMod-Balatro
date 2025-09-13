a6f1eeb3ad08debba648cd9801cccbffbeec04f1
CrucherOnion0z0 <satansALT2@gmail.com>
2025-09-13 13:51:13 +1200

    coin 'Nope!' text added

--- OnionJokers-main/src/relics.lua
+++ OnionJokers-main/src/relics.lua
@@ -911,7 +911,39 @@ SMODS.Consumable {
                     new_card:set_seal("Gold", nil, true)
                     new_card:set_edition("e_onio_gilded", true)
                     new_card:juice_up(0.3, 0.5)
+            else
+                G.E_MANAGER:add_event(Event({
+                    trigger = 'after',
+                    delay = 0.4,
+                    func = function()
+                        attention_text({
+                            text = localize('k_nope_ex'),
+                            scale = 1.3,
+                            hold = 1.4,
+                            major = card,
+                            backdrop_colour = G.C.SECONDARY_SET.Tarot,
+                            align = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK or G.STATE == G.STATES.SMODS_BOOSTER_OPENED) and
+                                'tm' or 'cm',
+                            offset = { x = 0, y = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK or G.STATE == G.STATES.SMODS_BOOSTER_OPENED) and -0.2 or 0 },
+                            silent = true
+                        })
+                        G.E_MANAGER:add_event(Event({
+                            trigger = 'after',
+                            delay = 0.06 * G.SETTINGS.GAMESPEED,
+                            blockable = false,
+                            blocking = false,
+                            func = function()
+                                play_sound('tarot2', 0.76, 0.4)
+                                return true
+                            end
+                        }))
+                        play_sound('tarot2', 1, 0.4)
+                        card:juice_up(0.3, 0.5)
+                        return true
+                    end
+                }))
             end
+            delay(0.5)
         return true end }))
     end,
     can_use = function(self, card)
